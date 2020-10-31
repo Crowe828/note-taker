9559c1c3fd59f64b78d050547701a9de091bcf2c
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-30 20:38:16 -0400

    created store.js file

---
+++ db/store.js
@@ -0,0 +1,60 @@
+Dependencies;
+const util = require("util");
+const fs = require("fs");
+// Generates unique IDs
+const uuidv1 = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      // If notes is not an array, send back an empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+
+    // Give all new notes unique IDs
+    const newNote = { title, text, id: uuidv1() };
+
+    // Get all notes, add new note, write updated notes, return a new note
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+
+  removeNote(id) {
+    // Remove individual notes by their ID
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
+}
+
+// Export it to be used elsewhere
+module.exports = new Store();
